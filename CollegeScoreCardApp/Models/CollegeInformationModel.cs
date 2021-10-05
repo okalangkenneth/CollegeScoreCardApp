164d24e2f50a31ce4b74508ca76d96c0a61459cc
Ken <okalang.ds@gmail.com>
2021-10-05 12:18:19 +0200

    Add project files.

---
+++ CollegeScoreCardApp/Models/CollegeInformationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CollegeScoreCardApp.Models
+{
+    public class CollegeInformationModel
+    {
+        public int Lateststudentsize { get; set; }
+        public string Schoolname { get; set; }
+        public string Schoolstate { get; set; }
+    }
+    
+}
